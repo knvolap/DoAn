9057381792dad666a79e2796215b5a57a0698ece
knvolap <58324366+knvolap@users.noreply.github.com>
2021-12-10 16:31:45 +0700

    10/12
    20%

---
+++ HiemMauNhanDao/HiemMauNhanDao/Areas/Client/Controllers/NguoiDungController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HiemMauNhanDao.Areas.Client.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        // GET: Client/NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- HiemMauNhanDao/Models/DAO/UserDao.cs
+++ HiemMauNhanDao/Models/DAO/UserDao.cs
@@ -53,6 +53,42 @@ namespace Models.DAO
             }
         }
 
+        public int Login2(string UserName, string PassWord)
+        {
+            // var Session= db.Quyens.SingleOrDefault(x => x.IdQuyen == id);
+
+            // return 0: Tài khoản hoặc mật khẩu không chính xác
+            // return 1: đăng nhập thành cônh
+            //return -1: tài khoản bị khoá
+            //retunrn -2: Không có quyền truy cập
+            var result = db.TaiKhoans.SingleOrDefault(x => x.userName == UserName);
+            if (result == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (result.idQuyen.Contains("Q03") || result.idQuyen.Contains("Q04") || result.idQuyen.Contains("Q05") || result.idQuyen.Contains("Q06"))
+                {
+                    if (result.trangThai == false) { return -1; }
+                    else
+                    {
+                        if (result.password == PassWord)
+                        {
+
+                            return 1;
+                        }
+                        else
+                            return 0;
+                    }
+                }
+                else
+                {
+                    return -2;
+                }
+            }
+        }
+
         public TaiKhoan GetById(string UserName)
         {
             return db.TaiKhoans.SingleOrDefault(x => x.userName == UserName);
