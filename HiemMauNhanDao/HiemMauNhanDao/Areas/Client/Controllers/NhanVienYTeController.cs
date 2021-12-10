688e987233a4264f999e5fff1f9ccd102ecd75b1
knvolap <58324366+knvolap@users.noreply.github.com>
2021-12-10 23:32:27 +0700

    10/12/02
    22%

---
+++ HiemMauNhanDao/HiemMauNhanDao/Areas/Client/Controllers/NhanVienYTeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HiemMauNhanDao.Areas.Client.Controllers
+{
+    public class NhanVienYTeController : Controller
+    {
+        // GET: Client/NhanVienYTe
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EditKQHM()
+        {
+            return View();
+        }
+    }
+}
--- HiemMauNhanDao/HiemMauNhanDao/Note/tk.cs
+++ HiemMauNhanDao/HiemMauNhanDao/Note/tk.cs
@@ -16,5 +16,14 @@ namespace HiemMauNhanDao.Note
 		//('TK07', 'Q06', 'nguoidung2@gmail.com','123123'),
 		//('TK08', 'Q06', 'nguoidung3@gmail.com','123123'),
 		//('TK09', 'Q06', 'nguoidung4@gmail.com','123123')	
+
+
+		////Bệnh viện 
+		//		+ Đăng ký đợt hiến máu -> update để sửa đơn vị liên kết 
+		//		+ đăng bài
+		//		+ đăng nhập bằng tk bệnh viện ->tạo tài khoản user 
+
+		////nhân viên
+		//		+ cập nhật kết quả hiến máu
     }
 }
